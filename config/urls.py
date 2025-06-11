4f5c06afdfd2ffd791bcc3d67d14f6be8f2d5d06
Felipe Carballo <felipecarballo1991@gmail.com>
2025-06-11 17:55:00 -0300

    Agrego scrapper Coto

---
+++ config/urls.py
@@ -0,0 +1,180 @@
+URLS = {
+    "bife_americano": {
+        "nombre": "Bife Americano",
+        "url": "https://www.cotodigital.com.ar/sitios/cdigi/productos/bife-americano-x-kg/_/R-00041414-00041414-200?Dy=1",
+        "unidad": "KG",
+        "supermercado": "Coto",
+        "categoria": "Carnes"
+    },
+    "ojo_bife": {
+        "nombre": "Ojo de Bife",
+        "url": "https://www.cotodigital.com.ar/sitios/cdigi/productos/ojo-de-bife-coto-xkg/_/R-00029810-00029810-200?Dy=1",
+        "unidad": "KG",
+        "supermercado": "Coto",
+        "categoria": "Carnes"
+    },
+    "bife_chorizo": {
+        "nombre": "Bife de Chorizo",
+        "url": "https://www.cotodigital.com.ar/sitios/cdigi/productos/bife-de-chorizo-coto-xkg/_/R-00029804-00029804-200?Dy=1",
+        "unidad": "KG",
+        "supermercado": "Coto",
+        "categoria": "Carnes"
+    },
+        "picada_desgrasada": {
+        "nombre": "Picada Desgrasada",
+        "url": "https://www.cotodigital.com.ar/sitios/cdigi/productos/picada-desgrasada-estancias-coto-x-kg/_/R-00048124-00048124-200?Dy=1",
+        "unidad": "KG",
+        "supermercado": "Coto",
+        "categoria": "Carnes"
+    },
+        "bife_parrilla": {
+        "nombre": "Bife de Parrilla",
+        "url": "https://www.cotodigital.com.ar/sitios/cdigi/productos/bife-parrilla-estancias-coto-x-kg/_/R-00048129-00048129-200?Dy=1",
+        "unidad": "KG",
+        "supermercado": "Coto",
+        "categoria": "Carnes"
+    },    
+        "pollo": {
+        "nombre": "Pollo Entero",
+        "url": "https://www.cotodigital.com.ar/sitios/cdigi/productos/pollo-congelado-x-kg/_/R-00042989-00042989-200?Dy=1",
+        "unidad": "KG",
+        "supermercado": "Coto",
+        "categoria": "Carnes"
+    },    
+        "papa_negra": {
+        "nombre": "Papa Negra",
+        "url": "https://www.cotodigital.com.ar/sitios/cdigi/productos/papa-negra-selecci%C3%B3n-x-kg/_/R-00060947-00060947-200?Dy=1",
+        "unidad": "KG",
+        "supermercado": "Coto",
+        "categoria": "Verdura"
+    },    
+        "cebolla": {
+        "nombre": "Cebolla",
+        "url": "https://www.cotodigital.com.ar/sitios/cdigi/productos/cebolla-a-granel-x-kg/_/R-00000602-00000602-200?Dy=1",
+        "unidad": "KG",
+        "supermercado": "Coto",
+        "categoria": "Verdura"
+    },    
+        "coliflor": {
+        "nombre": "Coliflor",
+        "url": "https://www.cotodigital.com.ar/sitios/cdigi/productos/coliflor-x-kg/_/R-00000619-00000619-200?Dy=1",
+        "unidad": "KG",
+        "supermercado": "Coto",
+        "categoria": "Verdura"
+    },    
+        "repollo_colorado": {
+        "nombre": "Repollo Colorado",
+        "url": "https://www.cotodigital.com.ar/sitios/cdigi/productos/repollo-colorado-x-kg/_/R-00000680-00000680-200?Dy=1",
+        "unidad": "KG",
+        "supermercado": "Coto",
+        "categoria": "Verdura"
+    }, 
+        "repollo_blanco": {
+        "nombre": "Repollo Blanco",
+        "url": "https://www.cotodigital.com.ar/sitios/cdigi/productos/repollo-blanco-x-kg/_/R-00000678-00000678-200?Dy=1",
+        "unidad": "KG",
+        "supermercado": "Coto",
+        "categoria": "Verdura"
+    },    
+        "zanahoria": {
+        "nombre": "Zanahoria",
+        "url": "https://www.cotodigital.com.ar/sitios/cdigi/productos/zanahoria-seleccion-xkg/_/R-00000686-00000686-200?Dy=1",
+        "unidad": "KG",
+        "supermercado": "Coto",
+        "categoria": "Verdura"
+    },
+      
+        "perejil": {
+        "nombre": "Perejil",
+        "url": "https://www.cotodigital.com.ar/sitios/cdigi/productos/perejil-x-uni/_/R-00047599-00047599-200?Dy=1",
+        "unidad": "Unidad",
+        "supermercado": "Coto",
+        "categoria": "Verdura"
+    },             
+        "lechuga_mantecosa": {
+        "nombre": "Lechuga Mantecosa",
+        "url": "https://www.cotodigital.com.ar/sitios/cdigi/productos/lechuga-mantecosa-x-kg/_/R-00000424-00000424-200?Dy=1",
+        "unidad": "KG",
+        "supermercado": "Coto",
+        "categoria": "Verdura"
+    }, 
+
+        "lechuga_capuchina": {
+        "nombre": "Lechuga Capuchina",
+        "url": "https://www.cotodigital.com.ar/sitios/cdigi/productos/lechuga-capuchina-x-kg/_/R-00000648-00000648-200?Dy=1",
+        "unidad": "KG",
+        "supermercado": "Coto",
+        "categoria": "Verdura"
+    },             
+        "lechuga_criolla": {
+        "nombre": "Lechuga Criolla",
+        "url": "https://www.cotodigital.com.ar/sitios/cdigi/productos/lechuga-criolla-x-kg/_/R-00000649-00000649-200?Dy=1",
+        "unidad": "KG",
+        "supermercado": "Coto",
+        "categoria": "Verdura"
+    },             
+        "lechuga_francesa": {
+        "nombre": "Lechuga Francesa",
+        "url": "https://www.cotodigital.com.ar/sitios/cdigi/productos/lechuga-francesa-x-kg/_/R-00000650-00000650-200?Dy=1",
+        "unidad": "KG",
+        "supermercado": "Coto",
+        "categoria": "Verdura"
+    },             
+        "brocoli": {
+        "nombre": "Brocoli",
+        "url": "https://www.cotodigital.com.ar/sitios/cdigi/productos/brocoli-x-kg/_/R-00000598-00000598-200",
+        "unidad": "KG",
+        "supermercado": "Coto",
+        "categoria": "Verdura"
+    },             
+        "jengibre": {
+        "nombre": "Jengibre",
+        "url": "https://www.cotodigital.com.ar/sitios/cdigi/productos/jengibre-x-kg/_/R-00092926-00092926-200",
+        "unidad": "KG",
+        "supermercado": "Coto",
+        "categoria": "Verdura"
+    },             
+        "pimiento_rojo": {
+        "nombre": "Pimiento Rojo",
+        "url": "https://www.cotodigital.com.ar/sitios/cdigi/productos/pimiento-rojo-xkg/_/R-00000671-00000671-200?Dy=1",
+        "unidad": "KG",
+        "supermercado": "Coto",
+        "categoria": "Verdura"
+    },             
+        "zapallo_anco": {
+        "nombre": "Zapallo Anco",
+        "url": "https://www.cotodigital.com.ar/sitios/cdigi/productos/zapallo-anco-x-kg/_/R-00000688-00000688-200?Dy=1",
+        "unidad": "KG",
+        "supermercado": "Coto",
+        "categoria": "Verdura"
+    },             
+        "remolacha": {
+        "nombre": "Remolacha",
+        "url": "https://www.cotodigital.com.ar/sitios/cdigi/productos/remolacha-x-kg/_/R-00000677-00000677-200",
+        "unidad": "KG",
+        "supermercado": "Coto",
+        "categoria": "Verdura"
+    },             
+        "tomate_redondo": {
+        "nombre": "Tomate Redondo",
+        "url": "https://www.cotodigital.com.ar/sitios/cdigi/productos/tomate-red-x-kg/_/R-00000684-00000684-200?Dy=1&assemblerContentCollection=%2Fcontent%2FShared%2FAuto-Suggest%20Panels",
+        "unidad": "KG",
+        "supermercado": "Coto",
+        "categoria": "Verdura"
+    },             
+        "tomate_perita": {
+        "nombre": "Tomate Perita",
+        "url": "https://www.cotodigital.com.ar/sitios/cdigi/productos/tomate-perit-xkg/_/R-00000683-00000683-200?Dy=1&assemblerContentCollection=%2Fcontent%2FShared%2FAuto-Suggest%20Panels",
+        "unidad": "KG",
+        "supermercado": "Coto",
+        "categoria": "Verdura"
+    }
+
+
+
+    }
+
+    
+    
+    
+    
