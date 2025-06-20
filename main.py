9ff32d199ee8ab74ca43b0eb38c19d3b3200d32d
Felipe Carballo <felipecarballo1991@gmail.com>
2025-06-19 21:00:03 -0300

    Agrego exportacion drive y modifico estructura de proyecto

---
+++ main.py
@@ -0,0 +1,10 @@
+from load.load import extract
+
+if __name__ == "__main__":
+    extract(
+        debug_export=False,        # Guardar localmente
+        debug_format=True,        # CSV
+        debug_info=False,         # URLs reales o debug
+        categoria="Limpiezas",    # Filtro opcional
+        debug_drive=True          # Subida a Google Sheet
+    )
