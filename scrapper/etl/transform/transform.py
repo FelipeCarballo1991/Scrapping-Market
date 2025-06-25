38717063bd1beaa2e492ea3b5990470fb1521804
Felipe Carballo <felipecarballo1991@gmail.com>
2025-06-25 18:11:01 -0300

    Ordeno columnas del resultado

--- scrapper/etl/transform/transform.py
+++ scrapper/etl/transform/transform.py
@@ -4,11 +4,27 @@ import pandas as pd
 # Métricas
 def generar_metricas(df):    
 
+    df = df[['fecha', 
+            'clave_config', 
+            'titulo_scrapeado', 
+            'precio', 
+            'unidad',
+            'supermercado', 
+            'categoria',    
+            'cant_unidades', 
+            'largo_mts',
+            'precio_rollo', 
+            'precio_metro',
+            'url', ]]
+
     df[['cant_unidades', 'largo_mts']] = df['unidad'].str.extract(r'(\d+)\s+Unidades\s+x\s+(\d+)\s+mts')
     df['cant_unidades'] = df['cant_unidades'].apply(lambda x: int(x) if pd.notna(x) else x)
     df['largo_mts'] = df['largo_mts'].apply(lambda x: int(x) if pd.notna(x) else x)
     df["precio_rollo"] = round(df["precio"] / df["cant_unidades"], 2)
-    df["precio_metro"] = round(df["precio_rollo"] / df["largo_mts"], 2)    
+    df["precio_metro"] = round(df["precio_rollo"] / df["largo_mts"], 2)   
+
+
+     
         
     return df
 
