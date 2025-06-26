689ae760a4bd5831e28908ad77b8e91d69df27d2
Felipe Carballo <felipecarballo1991@gmail.com>
2025-06-25 21:06:21 -0300

    Corrijo transform

--- scrapper/etl/transform/transform.py
+++ scrapper/etl/transform/transform.py
@@ -2,7 +2,14 @@
 import pandas as pd
 
 # Métricas
-def generar_metricas(df):    
+def generar_metricas(df):     
+
+    df[['cant_unidades', 'largo_mts']] = df['unidad'].str.extract(r'(\d+)\s+Unidades\s+x\s+(\d+)\s+mts')
+    df['cant_unidades'] = df['cant_unidades'].apply(lambda x: int(x) if pd.notna(x) else x)
+    df['largo_mts'] = df['largo_mts'].apply(lambda x: int(x) if pd.notna(x) else x)
+    df["precio_rollo"] = round(df["precio"] / df["cant_unidades"], 2)
+    df["precio_metro"] = round(df["precio_rollo"] / df["largo_mts"], 2)   
+
 
     df = df[['fecha', 
             'clave_config', 
@@ -16,14 +23,6 @@ def generar_metricas(df):
             'precio_rollo', 
             'precio_metro',
             'url', ]]
-
-    df[['cant_unidades', 'largo_mts']] = df['unidad'].str.extract(r'(\d+)\s+Unidades\s+x\s+(\d+)\s+mts')
-    df['cant_unidades'] = df['cant_unidades'].apply(lambda x: int(x) if pd.notna(x) else x)
-    df['largo_mts'] = df['largo_mts'].apply(lambda x: int(x) if pd.notna(x) else x)
-    df["precio_rollo"] = round(df["precio"] / df["cant_unidades"], 2)
-    df["precio_metro"] = round(df["precio_rollo"] / df["largo_mts"], 2)   
-
-
      
         
     return df
